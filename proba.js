a9def7a835dc46cc52b3ec75906e2a54e7eeb964
oldm12 <olga.dva3@gmail.com>
2024-08-16 14:13:28 +0300

    correct gitignor

--- proba.js
+++ proba.js
@@ -2,19 +2,22 @@
 
 const zaprosSlovar = require('osmosis');
 
-var urlSumInUA  = 'http://sum.in.ua/?swrd='
-// var urlSumInUA  = 'http://sum.in.ua/s/parta'
-
+// var urlSumInUA  = 'https://sum.in.ua/?swrd='
+var urlSumInUA  = 'https://sum.in.ua/s/'
+var urlSumInUAEx= 'https://sum.in.ua/s/parta'
 var zaprosSlovo = "parta"
 let otvetSlovar = [''];
 
+var zaprosSUMINUA = urlSumInUA+zaprosSlovo
+console.log(zaprosSUMINUA)
 
 zaprosSlovar
-.get(urlSumInUA+zaprosSlovo)
+.get(zaprosSUMINUA)
 .set({'Title': 'div#article'})
 .data(function (dataOtvetSlovar) {
   otvetSlovar.push(dataOtvetSlovar);
   })
 .done(function (){
+  console.log(otvetSlovar)
 //  otvetSlovar;
   })
